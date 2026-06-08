88eb36e3038213f0e2b68be4f28c7b9a5ea407a5
Heck_ClearVision_ <abdxllahmazharx@gmail.com>
2026-06-09 01:52:07 +0500

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -1,3 +1,9 @@
-fun main(){
-  println("Hello World")
+fun main() {
+    println("What is your name?")
+    var name = readLine()!!
+
+    println("What is your age?")
+    var age = readLine()!!.toInt()
+
+    println("My name is $name and I am $age years old")
 }
